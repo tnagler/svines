fe2e250abcd5efbfb2ff68403a37336b7359ca1d
tnagler <thomas.nagler@tum.de>
2020-07-10 20:22:50 +0200

    faster TSP

--- R/tools_select.R
+++ R/tools_select.R
@@ -1,25 +1,32 @@
-#' @importFrom TSP TSP insert_dummy solve_TSP cut_tour
 find_hamilton_path <- function(u) {
-  d0 <- ncol(u)
-  w <- 1 - abs(wdm::wdm(cbind(u[-1, ], u[-nrow(u), ]), method = "kendall"))
-  tsp <- TSP(w[1:d0, 1:d0])
-  hamilton <- insert_dummy(tsp, label = "cut")
+  d <- NCOL(u)
+  W <- abs(wdm::wdm(u, method = "kendall"))
+  diag(W) <- 0
+  order <- which.max(apply(W, 2, max))
+  for (k in seq_len(d - 1)) {
+    ind <- c(order[1], order[k])
+    m <- apply(W[, ind], 2, max)
+    v <- apply(W[, ind], 2, which.max)
+    imax <- which.max(m)
+    W[order, v[imax]] <- W[v[imax], order] <- 0
+    order <- if (imax == 1) c(v[imax], order) else c(order, v[imax])
+  }
   
-  # withSeed to make it reproducible
-  sol <- R.utils::withSeed(solve_TSP(hamilton, method = "repetitive_nn"), 5)
-  as.numeric(cut_tour(sol, "cut"))
+  unname(order)
 }
-
 
 select_mvine <- function(u) {
   d0 <- ncol(u)
   css <- rvinecopulib::dvine_structure(find_hamilton_path(u))
-  tau <- wdm::wdm(u[-nrow(u), css$order[c(1, d0)]],  # t
-                  u[-1, css$order[c(1, d0)]],        # t + 1
-                  method = "kendall")
-  
-  if (abs(tau[1, 1]) < abs(tau[2, 2]))
+  tau <- wdm::wdm(
+    u[-nrow(u), css$order[c(1, d0)]], # t
+    u[-1, css$order[c(1, d0)]],       # t + 1
+    method = "kendall"
+  )
+
+  if (abs(tau[1, 1]) < abs(tau[2, 2])) {
     css$order <- rev(css$order)
+  }
   list(
     cs_structure = css,
     out_vertices = css$order,
@@ -30,12 +37,15 @@ select_mvine <- function(u) {
 select_dvine <- function(u) {
   d0 <- ncol(u)
   css <- rvinecopulib::dvine_structure(find_hamilton_path(u))
-  tau <- wdm::wdm(u[-nrow(u), css$order[c(1, d0)]],  # t
-                  u[-1, css$order[c(1, d0)]],        # t + 1
-                  method = "kendall")
+  tau <- wdm::wdm(
+    u[-nrow(u), css$order[c(1, d0)]], # t
+    u[-1, css$order[c(1, d0)]],       # t + 1
+    method = "kendall"
+  )
 
-  if (abs(tau[1, 2]) > abs(tau[2, 1]))
+  if (abs(tau[1, 2]) > abs(tau[2, 1])) {
     css$order <- rev(css$order)
+  }
   list(
     cs_structure = css,
     out_vertices = rev(css$order),
@@ -53,19 +63,19 @@ allowed_margin_families <- get_allowed_margin_families()
 
 select_margin <- function(x, families, criterion, na.rm = FALSE) {
   mlf <- sapply(
-    paste0("univariateML::ml", families), 
+    paste0("univariateML::ml", families),
     function(x) eval(parse(text = x))
   )
   fits <- lapply(mlf, function(f) try(f(x, na.rm = na.rm), silent = TRUE))
-  
+
   ## catch out-of-bounds errors (and similar)
   error_inds <- sapply(fits, function(fit) inherits(fit, "try-error"))
   error_msgs <- sapply(fits[error_inds], as.character)
   if (all(error_inds)) {
-    details <- paste0("(", names(error_msgs), ") ",  error_msgs)
+    details <- paste0("(", names(error_msgs), ") ", error_msgs)
     stop("couldn't fit any model.\n", details)
   }
-  
+
   ## select best model
   fits <- fits[!error_inds]
   crits <- switch(
@@ -77,4 +87,3 @@ select_margin <- function(x, families, criterion, na.rm = FALSE) {
   )
   fits[[which.min(crits)]]
 }
-
