04bdb16a6380f7a2deb73aa09b4c54093d1eaba7
tnagler <mail@tnagler.com>
2022-03-14 18:57:30 +0100

    trigger check

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // svinecop_create_cpp
 Rcpp::List svinecop_create_cpp(const Rcpp::List& svine_r);
 RcppExport SEXP _svines_svinecop_create_cpp(SEXP svine_rSEXP) {
