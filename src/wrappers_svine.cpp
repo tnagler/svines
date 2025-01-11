e8dd271f518a50a7b6dad733d40eab7c56480734
tnagler <thomas.nagler@tum.de>
2025-01-11 18:48:39 +0100

    wrap structure

--- src/wrappers_svine.cpp
+++ src/wrappers_svine.cpp
@@ -113,14 +113,16 @@ svinecop_select_cpp(const Eigen::MatrixXd& data,
   fit_controls.set_num_threads(num_threads);
   
   SVinecop svine(var_types.size(), p, var_types);
-  // if (is_structure_provided) {
+  if (is_structure_provided) {
+    rvine_structure_wrap(structure, false);
   //   svine = SVinecop(rvine_structure_wrap(structure, false),
   //                    p,
   //                    out_vertices,
   //                    in_vertices,
   //                    var_types);
   //   svine.select_families(data, fit_controls);
-  // } else {
+  }
+  // else {
     svine.select_all(data, fit_controls);
   // }
 
